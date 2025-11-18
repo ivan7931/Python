9d3be586421968fc2658cd0ee8fbc25e39920b1f
ivan7931 <ivanivann34@gmail.com>
2025-11-18 11:03:08 +0100

    bdfs

---
+++ Ej_Funciones2/Ej_1/Ej_1.py
@@ -0,0 +1,10 @@
+def areaCirculo(radio):
+    area = float(3.14*(radio**2))
+    return area
+
+def areaCuadrado(lado):
+    area = float(lado**2)
+    return area
+def areaTriangulo(base, altura):
+    area = (base*altura)/2
+    return area
---
+++ Ej_Funciones2/Ej_1/EjecutorEj_1.py
@@ -0,0 +1,5 @@
+import Ej_1 as ejercicio
+
+print(ejercicio.areaCirculo(5))
+print(ejercicio.areaCuadrado(10))
+print(ejercicio.areaTriangulo(4,6))
